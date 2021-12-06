fdc2e86a2daf847dced1712abb77031201334475
prasadaraeddy anam <anam.prasadreddy@gmail.com>
2021-12-06 11:00:35 +0530

    [Merge Conflict Resolved]

--- src/com/bnp/git/testing/MyTestClass.java
+++ src/com/bnp/git/testing/MyTestClass.java
@@ -3,6 +3,7 @@ package com.bnp.git.testing;
 public class MyTestClass {
 	public static void main(String[] args) {
 		System.out.println("Hello I Am A Test Class. AABC");
+		System.out.println("Hello I Am A Test Class. 12345");
 	}
 
 }
